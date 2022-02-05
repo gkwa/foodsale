4fde1afe30894de7e339b1e5ea58f1e47da21757
Your Name <you@example.com>
2022-02-05 13:53:17 -0800

    real men dont make git commit comments

--- src/foodsale/pathfromglob.py
+++ src/foodsale/pathfromglob.py
@@ -4,12 +4,12 @@ from typing import List
 
 
 def abspathglob(*sglobs) -> List[pathlib.Path]:
-    cwd = pathlib.Path.cwd()
 
     paths = set()
     for _str in sglobs:
         absolute = pathlib.Path(_str)
         relative = str(absolute).lstrip(absolute.anchor)
+        cwd = pathlib.Path.cwd()
         try:
             os.chdir(absolute.anchor)
             glob = pathlib.Path(absolute.anchor).glob(relative)
