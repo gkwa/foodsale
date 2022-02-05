df74fa5b61541e76d08a303cb4ecbb50578a9168
Your Name <you@example.com>
2022-02-05 11:05:50 -0800

    redo

--- src/foodsale/pathfromglob.py
+++ src/foodsale/pathfromglob.py
@@ -1,104 +1,30 @@
 import logging
 import os
 import pathlib
+from dataclasses import dataclass, field
 from typing import List
 
 
-class NonRelativeGlob:
-    def __init__(self, _str):
-        self.glob = None
-        self.cwd = pathlib.Path.cwd()
-        self._str = _str
-        self.drive = pathlib.Path(_str).drive
-        self.path = None
-        self.initialize(_str)
-
-    def initialize(self, _str):
-        try:
-            p1 = pathlib.PureWindowsPath(self._str)
-            logging.debug(type(p1))
-            logging.debug(p1)
-            logging.debug(f"{p1.drive=}")
-            logging.debug(f"{p1.root=}")
-            x = str(p1).lstrip(p1.drive)
-            logging.debug(x)
-            x = str(x).lstrip(p1.root)
-            logging.debug(f"{x=}")
-            root = pathlib.Path("/")
-            logging.debug(f"{root=}")
-            z = f"*{x}"
-            logging.debug(f"{z=}")
-            if self.drive:
-                os.chdir(p1.drive)
-            y = pathlib.Path("/").glob(z)  # y is a generator
-            logging.debug(f"{y=}")
-            paths = list(y)  # consumes the generator
-            logging.debug(f"{paths=}")
-            if not paths:
-                logging.warning("couldn't find a match for {}".format(self._str))
-            else:
-                # naively choose one with higest version number
-                last = paths[-1]
-                last = last.resolve()
-                logging.debug("using file with path {}".format(last))
-                self.path = last
-
-        except BaseException as ex:
-            logging.exception(ex)
-            raise
-
-        finally:
-            os.chdir(self.cwd)
+def abspathglobs(globstrings):
+    paths = []
+    for _str in globstrings:
+        paths.append(abspathglob(_str))
 
 
-class PathFromGlob:
-    """
-    # multiline string like this
-
-    C:/Program*/Microsoft SDKs/Windows/v[!6]*/Bin/SignTool.exe
-    C:/Program*/Windows Kits/*/bin/*/*/SignTool.exe
-
-    or single glob like this:
-    C:/Program*/Windows Kits/*/bin/*/*/SignTool.exe
-
-    or this:
-    */Program*/Windows Kits/*/bin/*/*/SignTool.exe
-    """
-
-    def __init__(self, _str):
-        logging.debug(_str)
-        self.root = None
-        self.globs_str = _str
-        self.path = None
-        self.initialize(_str)
-
-    def multiline(self, _str) -> List[pathlib.Path]:
-        candidates = []
-        for line in _str.splitlines():
-            clean = line.strip()
-            if not clean:
-                continue
-            nrg = NonRelativeGlob(clean)
-            logging.debug(nrg.path)
-            if nrg.path:
-                candidates.append(nrg.path)
-        return candidates
-
-    def initialize(self, _str):
-        candidates = self.multiline(_str)
-
-        if candidates:
-            self.path = candidates[-1]
-
-    @classmethod
-    def from_string(cls, _str):
-        return cls(_str)
+def abspathglob(_str) -> List[pathlib.Path]:
+    p1 = pathlib.Path(_str)
+    p2 = str(p1).lstrip(p1.anchor)
+    cwd = pathlib.Path.cwd()
+    try:
+        os.chdir(p1.anchor)
+        p3 = pathlib.Path(p1.anchor).glob(p2)
+    finally:
+        os.chdir(cwd)
+    return p3
 
 
 def main():
-    _str = r"C:\Program*\WiX Toolset*\bin\heat.exe"
-    p1 = PathFromGlob(_str)
-    print(p1)
+    pass
 
 
 if __name__ == "__main__":
--- tests/test_pathfromglob.py
+++ tests/test_pathfromglob.py
@@ -1,57 +1,27 @@
+import logging
 import pathlib
+import sys
+import tempfile
+
+import pytest
 
 from foodsale import pathfromglob
 
+if not sys.platform.startswith("win"):
+    pytest.skip("only windows", allow_module_level=True)
+
+
 """
->>> import pathlib
->>> import os
->>>
->>> _str = r"C:\Program*\WiX Toolset*\bin\heat.exe"
->>> 
->>> x = str(p1).lstrip(p1.drive)
->>> x = str(x).lstrip(p1.root)
->>> root = pathlib.Path("/")
->>> z = f"*{x}"
->>> print(z)
-*Program*\WiX Toolset*\bin\heat.exe
->>> os.chdir("c:")
->>> y = pathlib.Path("/").glob(z)
->>> print(y)
-<generator object Path.glob at 0x0000026FE9F96820>
->>> print(list(y))
-[WindowsPath('/Program Files (x86)/WiX Toolset v3.11/bin/heat.exe')]
->>>
+or this:
+*/Program*/Windows Kits/*/bin/*/*/SignTool.exe
 """
 
 
-def test1():
-    _str = r"C:\Program*\WiX Toolset*\bin\heat.exe"
-    p1 = pathfromglob.PathFromGlob(_str)
-
-
-def test():
-    x = "C:/Program*/Windows Kits/*/bin/*/*/SignTool.exe"
-    y = pathfromglob.PathFromGlob(x)
-    print(y)
-
-
-def test_pathfromglob():
-    def rmtree(root):
-        if not root.exists():
-            return
-
-        for p in root.iterdir():
-            if p.is_dir():
-                rmtree(p)
-            else:
-                p.unlink()
-        root.rmdir()
-
-    root = pathlib.Path("/tmp/signit_test_1")
-    rmtree(root)
-
-    test = root / "test.txt"
-    test.parent.mkdir(parents=True, exist_ok=True)
-    test.write_text("")
-
-    path = pathfromglob.PathFromGlob.from_string("t*/sig*/test.txt").path
+def windows_test1():
+    base_dir = pathlib.Path(tempfile.gettempdir())
+    y1 = base_dir / "Program Files\WiX Toolset 3.11\bin\heat.exe"
+    y1.parent.mkdir(parents=True, exist_ok=True)
+    y1.touch()
+    glob = pathfromglob.abspathglob(str(y1))
+    result = list(glob)
+    assert len(result) == 1
