b8a47afe88b26d28f25fcef75bc12dcd08d0cac5
Your Name <you@example.com>
2022-02-05 13:41:50 -0800

    real men dont make git commit comments

--- tests/test_pathfromglob.py
+++ tests/test_pathfromglob.py
@@ -15,16 +15,20 @@ if not sys.platform.startswith("win"):
     pytest.skip("skipping windows-only tests", allow_module_level=True)
 
 
-"""
-or this:
-*/Program*/Windows Kits/*/bin/*/*/SignTool.exe
-"""
-
-
 def test_windows_single_glob_string():
     base_dir = pathlib.Path(tempfile.gettempdir())
     y1 = base_dir / r"Program Files\WiX Toolset 3.11\bin\heat.exe"
     p1 = base_dir / r"Program*\WiX Toolset*\*\heat.exe"
+    y1.parent.mkdir(parents=True, exist_ok=True)
+    y1.touch()
+    lst = pathfromglob.abspathglob(str(p1))
+    assert len(lst) == 1
+
+
+def test_windows_single_glob_string_two():
+    base_dir = pathlib.Path(tempfile.gettempdir())
+    y1 = base_dir / r"Program Files\WiX Toolset 3.11\bin\heat.exe"
+    p1 = base_dir / r"Program*/WiX Toolset*/*/heat.exe"
     y1.parent.mkdir(parents=True, exist_ok=True)
     y1.touch()
     lst = pathfromglob.abspathglob(str(p1))
