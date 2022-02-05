b70ce61ebe3e7a4fad0defe2a92cb70cb5aac144
Your Name <you@example.com>
2022-02-05 11:21:54 -0800

    real men dont make git commit comments

--- tests/test_pathfromglob.py
+++ tests/test_pathfromglob.py
@@ -13,8 +13,8 @@ __copyright__ = "Taylor Monacelli"
 __license__ = "MPL-2.0"
 
 
-if sys.platform.startswith("win"):
-    pytest.skip("only windows", allow_module_level=True)
+if not sys.platform.startswith("win"):
+    pytest.skip("skipping windows-only tests", allow_module_level=True)
 
 
 """
