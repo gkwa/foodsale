694139c9e3e7686e1ae51f3e2dc20250f3c09fda
Your Name <you@example.com>
2022-02-05 12:19:07 -0800

    real men dont make git commit comments

--- src/foodsale/pathfromglob.py
+++ src/foodsale/pathfromglob.py
@@ -5,10 +5,10 @@ from typing import List
 
 def abspathglob(*sglobs) -> List[pathlib.Path]:
     paths = set()
+    cwd = pathlib.Path.cwd()
     for _str in sglobs:
         p1 = pathlib.Path(_str)
         p2 = str(p1).lstrip(p1.anchor)
-        cwd = pathlib.Path.cwd()
         try:
             os.chdir(p1.anchor)
             g = pathlib.Path(p1.anchor).glob(p2)
@@ -16,7 +16,8 @@ def abspathglob(*sglobs) -> List[pathlib.Path]:
                 paths.add(path)
         finally:
             os.chdir(cwd)
-        return list(paths)
+
+    return list(paths)
 
 
 def main():
